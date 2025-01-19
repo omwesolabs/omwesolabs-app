d2743b0c231f5a5fd80245358b933e4b0123cbf6
Joshua Ogwang <joshua.ogwang@outlook.com>
2025-01-19 20:20:21 +0300

    feat(oauth-integration): add route middleware to protect pages

---
+++ src/app/middleware.js
@@ -0,0 +1,23 @@
+import {NextResponse} from "next/server";
+import {createMiddlewareClient} from "@supabase/auth-helpers-nextjs";
+
+const publicPath = ['/', '/about', '/contact', '/login', '/support', '/careers']
+
+export async function middleware(req) {
+    const res = NextResponse.next()
+    const supabase = createMiddlewareClient({req, res})
+    const {data: {session}, error} = await supabase.auth.getSession()
+    const isPublicPath = publicPath.some(path => req.nextUrl.pathname.startsWith(path))
+    if (!session && !isPublicPath) {
+        const redirectUrl = new URL("/login", req.url)
+        redirectUrl.searchParams.set('redirectTo', req.nextUrl.pathname)
+        return NextResponse.redirect(redirectUrl)
+    }
+    return res
+}
+
+export const config = {
+    matcher: [
+        '/((?!_next/static|_next/image|favicon.ico|public/).*)'
+    ]
+}
